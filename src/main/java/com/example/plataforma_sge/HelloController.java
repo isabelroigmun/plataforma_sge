139661505c2d298a90b995e03947f8e6ffe8faff
isabelroigmun <isabelroimun@gmail.com>
2026-01-18 17:11:19 +0100

    ocultar contraseña inicio sesión

--- src/main/java/com/example/plataforma_sge/HelloController.java
+++ src/main/java/com/example/plataforma_sge/HelloController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -19,7 +20,7 @@ public class HelloController {
     private TextField user;
 
     @FXML
-    private TextField password;
+    private PasswordField password;
 
     @FXML
     protected void signin() throws IOException {
