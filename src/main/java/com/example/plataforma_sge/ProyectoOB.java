0eb121a875bbdfe58737679b4bc190a8296aad74
isabelroigmun <isabelroimun@gmail.com>
2026-01-18 21:10:40 +0100

    actualizo ProyectoOB.java

--- src/main/java/com/example/plataforma_sge/ProyectoOB.java
+++ src/main/java/com/example/plataforma_sge/ProyectoOB.java
@@ -1,26 +1,43 @@
 package com.example.plataforma_sge;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class ProyectoOB {
 
-Integer codigo;
-String nombre;
-String tipo;
-boolean estado;
-int jefe;
+    private int codigo;
+    private String nombre;
+    private LocalDate fechaCreacion;
+    private LocalDate fechaEjecucion;
+    private String palabrasClave;
+    private String tipo;
+    private String estado;
+    private String calificacion;
+    private boolean bajadaCalificacion;
+    private boolean enCooperacion;
+    private int fases;
+    private int jefeId;
 
-    public ProyectoOB(Integer codigo, String nombre, String tipo, boolean estado, int jefe) {
+    public ProyectoOB(int codigo, String nombre, LocalDate fechaCreacion, LocalDate fechaEjecucion, String palabrasClave, String tipo, String estado, String calificacion, boolean bajadaCalificacion, boolean enCooperacion, int fases, int jefeId) {
         this.codigo = codigo;
         this.nombre = nombre;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaEjecucion = fechaEjecucion;
+        this.palabrasClave = palabrasClave;
         this.tipo = tipo;
         this.estado = estado;
-        this.jefe = jefe;
+        this.calificacion = calificacion;
+        this.bajadaCalificacion = bajadaCalificacion;
+        this.enCooperacion = enCooperacion;
+        this.fases = fases;
+        this.jefeId = jefeId;
     }
 
-    public Integer getCodigo() {
+    public int getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(Integer codigo) {
+    public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
 
@@ -32,6 +49,30 @@ int jefe;
         this.nombre = nombre;
     }
 
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDate getFechaEjecucion() {
+        return fechaEjecucion;
+    }
+
+    public void setFechaEjecucion(LocalDate fechaEjecucion) {
+        this.fechaEjecucion = fechaEjecucion;
+    }
+
+    public String getPalabrasClave() {
+        return palabrasClave;
+    }
+
+    public void setPalabrasClave(String palabrasClave) {
+        this.palabrasClave = palabrasClave;
+    }
+
     public String getTipo() {
         return tipo;
     }
@@ -40,19 +81,51 @@ int jefe;
         this.tipo = tipo;
     }
 
-    public boolean isEstado() {
+    public String getEstado() {
         return estado;
     }
 
-    public void setEstado(boolean estado) {
+    public void setEstado(String estado) {
         this.estado = estado;
     }
 
-    public int getJefe() {
-        return jefe;
+    public String getCalificacion() {
+        return calificacion;
     }
 
-    public void setJefe(int jefe) {
-        this.jefe = jefe;
+    public void setCalificacion(String calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public boolean isBajadaCalificacion() {
+        return bajadaCalificacion;
+    }
+
+    public void setBajadaCalificacion(boolean bajadaCalificacion) {
+        this.bajadaCalificacion = bajadaCalificacion;
+    }
+
+    public boolean isEnCooperacion() {
+        return enCooperacion;
+    }
+
+    public void setEnCooperacion(boolean enCooperacion) {
+        this.enCooperacion = enCooperacion;
+    }
+
+    public int getFases() {
+        return fases;
+    }
+
+    public void setFases(int fases) {
+        this.fases = fases;
+    }
+
+    public int getJefeId() {
+        return jefeId;
+    }
+
+    public void setJefeId(int jefeId) {
+        this.jefeId = jefeId;
     }
 }
