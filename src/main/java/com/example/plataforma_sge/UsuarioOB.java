46c3e50dfd6431a8000487f8aac7da418cb1023a
isabelroigmun <isabelroimun@gmail.com>
2026-01-23 16:52:20 +0100

    creo clase UsuarioOB.java

---
+++ src/main/java/com/example/plataforma_sge/UsuarioOB.java
@@ -0,0 +1,73 @@
+package com.example.plataforma_sge;
+
+public class UsuarioOB {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String usuario;
+    private String contra;
+    private int rol;
+
+    public UsuarioOB(int id, String nombre, String apellidos, String usuario, String contra, int rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.usuario = usuario;
+        this.contra = contra;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return getUsuario();
+    }
+
+}
