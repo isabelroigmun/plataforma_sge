234bb927795b3c26a611fbd68672dd1487f178c4
isabelroigmun <isabelroimun@gmail.com>
2026-02-12 18:05:42 +0100

    actualización de inicio de la base de datos

--- bdd/data.sql
+++ bdd/data.sql
@@ -5,3 +5,8 @@ VALUES ("Administrador","Encargado de poder manipular las opciones dentro de la 
 
 INSERT INTO usuarios (nombre, apellidos,usuario, contraseña, rol_id)
 VALUES ("prueba","prueba","prueba","prueba",1);
+
+INSERT INTO roles (nombre, descripcion)
+VALUES ("Administrador","Encargado de poder manipular las opciones dentro de la app"),
+("Lectura","Usuario con permisos solo de lectura."),
+("Escritura","Usuario con permisos de lectura y escritura.");
